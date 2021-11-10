789253aef69431782bd3af5efe1669fd2367160c
Dylan Tan <86589275+DIVlNE@users.noreply.github.com>
2021-11-10 23:58:11 +0800

    Add files via upload
    Added Reset Button Function 
    - It will reset the value to 0 instead of text.content only.
    - Css style button

--- script/index.js
+++ script/index.js
@@ -1,19 +1,21 @@
-// document.getElementById("count-el").innerText = 5
-
-// let count = 5
-// count = count + 1
-// console.log(count)
-
-// camelCase
-let countEl = document.getElementById("count-el") // pass in arguments
-
-console.log(countEl)
-
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
 let count = 0
+let resetButton = document.querySelector("#reset-btn")
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+    count += 1
+    countEl.textContent = count
 }
 
 
+function save() {
+    let countStr = count + " - " 
+    saveEl.textContent += countStr
+}
+
+function reset() {
+    count = 0;
+    document.getElementById('count-el').innerHTML = 0;
+}
